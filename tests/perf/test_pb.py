2fe876b7725f80cae58b7f0fd66f0932522152e9
harshpsuthar <harsh-pravinkumar.suthar@mohawkcollege.ca>
2022-03-04 11:20:19 -0500

    updated

--- tests/perf/test_pb.py
+++ tests/perf/test_pb.py
@@ -15,18 +15,5 @@ class TestClasss:
         result = posts.get_1st_todo()
         assert result.status_code == 200
 
-    # @allure.step("Test2 execution start")
-    # def test_get_token2(self):
-    #     result = posts.generate_token()
-    #     assert result.status_code == 200
-    #
-    # @allure.step("Test3 execution start")
-    # def test_get_token3(self):
-    #     result = posts.generate_token()
-    #     assert result.status_code == 200
-    #
-    # @allure.step("Test4 execution start")
-    # def test_get_token4(self):
-    #     result = posts.generate_token()
-    #     assert result.status_code == 200
+
 
