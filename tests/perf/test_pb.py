c4e72225da923ee4eb25e32c4dab69dec28dc569
Harsh Suthar <37757303+harsh-suthar@users.noreply.github.com>
2022-03-11 12:28:53 -0500

    update

--- tests/perf/test_pb.py
+++ tests/perf/test_pb.py
@@ -1,11 +1,12 @@
 import allure
 from helpers import call as posts
+from helpers.allure import allureHelper
 import pytest
 
 @pytest.mark.usefixtures("set_up")
 class TestWeb:
-    def test_odin(self):
-        pass
+    def test_odin(self,odin):
+        allure.attach((odin), name='Odin', attachment_type=self.allure.attachment_type.TEXT)
 
 
 
