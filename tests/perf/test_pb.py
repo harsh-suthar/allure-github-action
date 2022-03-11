559d90f9f63a2abe24cbe407b77385e4aba7d9d1
Harsh Suthar <37757303+harsh-suthar@users.noreply.github.com>
2022-03-11 11:30:36 -0500

    Update test_pb.py

--- tests/perf/test_pb.py
+++ tests/perf/test_pb.py
@@ -5,15 +5,10 @@ from helpers import call as posts
 
 class TestClasss:
 
-    @allure.step("Test1 execution start")
-    def test_get_token1(self):
-        result = posts.generate_token()
-        assert result.status_code == 200
+    class TestWeb(WebBaseClass):
+    def test_odin(self):
+        pass
 
-    @allure.step("Test1 execution start")
-    def test_get_token2(self):
-        result = posts.get_1st_todo()
-        assert result.status_code == 200
 
 
 
