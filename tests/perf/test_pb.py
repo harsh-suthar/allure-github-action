df7b17dd3648e18245a6fe015e20ea8a120f4df1
Harsh Suthar <37757303+harsh-suthar@users.noreply.github.com>
2022-03-23 13:43:47 -0400

    Update test_pb.py

--- tests/perf/test_pb.py
+++ tests/perf/test_pb.py
@@ -11,4 +11,12 @@ class TestWeb:
 
 
 
+    @allure.step("Test1 execution start")
+    def test_get_token1(self):
+        result = posts.generate_token()
+        assert result.status_code == 200
 
+    @allure.step("Test1 execution start")
+    def test_get_token2(self):
+        result = posts.get_1st_todo()
+        assert result.status_code == 200
