e668f34c18d9a76c10584b201e7d44b5640adf73
harshpsuthar <harsh-pravinkumar.suthar@mohawkcollege.ca>
2022-04-06 11:21:50 -0400

    Update installChrome.sh

--- scripts/installChrome.sh
+++ scripts/installChrome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget -O firefox-stable https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US
+wget -O firefox-stable.exe https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US
 sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt install ./firefox-stable.tar.bz2
+sudo apt-get install wine
+sudo wine ./firefox-stable.exe
